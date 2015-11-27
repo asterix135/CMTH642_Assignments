3802e0dd524d080c0cf0b2c4f26dbf5390d511d1
asterix135 <chgraham@gmail.com>
2015-11-27 08:27:44 -0500

    Created Assignment 3

---
+++ Assignment3/hw3_v1.R
@@ -0,0 +1,8 @@
+# Data load & EDA
+
+# Set working directory
+if (getwd() != "/Users/christophergraham/Documents/School/CMTH642/CMTH642_Assignments/Assignment3") {
+    setwd("/Users/christophergraham/Documents/School/CMTH642/CMTH642_Assignments/Assignment3")
+}
+
+# load 
