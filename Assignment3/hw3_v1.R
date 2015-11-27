3cbdf027c16e0d24fca8c8bf148a07507a5bee62
asterix135 <chgraham@gmail.com>
2015-11-27 17:47:58 -0500

    started some

--- Assignment3/hw3_v1.R
+++ Assignment3/hw3_v1.R
@@ -1,8 +1,25 @@
 # Data load & EDA
 
+require(caret)
+
 # Set working directory
 if (getwd() != "/Users/christophergraham/Documents/School/CMTH642/CMTH642_Assignments/Assignment3") {
     setwd("/Users/christophergraham/Documents/School/CMTH642/CMTH642_Assignments/Assignment3")
 }
 
-# load 
+# load data
+
+red <- read.csv('winequality-red.csv', sep = ';')
+
+# basic info
+sum(complete.cases(red))
+cor(red)
+table(red$quality)
+
+# split off test data for final confirmation
+set.seed(259678)
+valid_idx <- createDataPartition(y=red$quality, p=0.1, list = FALSE)
+training <- red[-valid_idx,]
+validation <- red[valid_idx,]
+table(validation$quality)
+table(training$quality)
