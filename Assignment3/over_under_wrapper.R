0b230e9132e9a09712b77f73862e9139b07a06eb
asterix135 <chgraham@gmail.com>
2015-11-30 22:27:54 -0500

    model built but overfits

--- Assignment3/over_under_wrapper.R
+++ Assignment3/over_under_wrapper.R
@@ -4,17 +4,26 @@
 # Knowledge Discovery*. 2008;17(2):225-52.
 
 
-wrapper <- function(MinorList, MajorList, NumFolds) {
+wrapper <- function(MinorList, MajorList, evalFUN) {
     # Set all MajorList undersample values to 100%
     UnderSampleList <- rep(1, times = length(MajorList))
     names(UnderSampleList) <- MajorList
     # Set all Minority Class Smote values to 0%
     SmoteList <- rep(0, times=length(MinorList))
     names(SmoteList) <- MinorList
-    # 
+    # Get base f values
+    
+    # Run undersample loop
+    if (length(MajorList) > 0) {
+        undersample(MajorList, MinorList, evalFUN)
+    }
+    # Run smote loop
+    if (length(MinorList) > 0) {
+        smote(MinorList, evalFUN)
+    }
 }
 
-undersample <- function(dunno) {
+undersample <- function(MajorList, evalFUN) {
     pass
 }
 
