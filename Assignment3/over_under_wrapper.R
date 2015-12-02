5fbb557e5ebd94b3e960685a66287e82511dd3ca
asterix135 <chgraham@gmail.com>
2015-12-02 08:14:59 -0500

    smote wrapper written
    need to write smote function and update performance measures

--- Assignment3/over_under_wrapper.R
+++ Assignment3/over_under_wrapper.R
@@ -5,6 +5,10 @@
 
 require(caret)
 
+
+# need to change mertric to f-measure
+# (2 x precision x recall) / (recall + precision)
+
 wrapper <- function(class_list, train_set, eval_fun, seed) {
     # class list is a named vector with -1=Minority, 0 = Neutral; 1 = Majority
     # train_set is a data frame with full training data
@@ -13,8 +17,10 @@ wrapper <- function(class_list, train_set, eval_fun, seed) {
     num_class <- length(class_list)
     
     # class_data will manage information on classes
-    class_data <- data.frame(rbind(maj = class_list == 1))
+    class_data <- data.frame(rbind(orig = class_list))
     colnames(class_data) <- names(class_list)
+    
+    class_data <- rbind(class_data, maj = class_list == 1)
     class_data <- rbind(class_data, min = class_list == -1)
     
     # Set all MajorList undersample values to 100%
@@ -48,7 +54,7 @@ wrap_undersample <- function(class_data, train_data, eval_fun, seed) {
     # Set StopSampling Flag to False
     class_data <- rbind(class_data, stop_sampling = !class_data['maj',])
     # main loop
-    while (sum(class_data['stop_sampling'] > 0)) {
+    while (sum(class_data['stop_sampling'] < ncol(class_data))) {
         for (i in 1:ncol(class_data)) {
             if (class_data['stop_sampling',i] == FALSE) {
                 train_len = nrow(train_data)
@@ -70,8 +76,8 @@ wrap_undersample <- function(class_data, train_data, eval_fun, seed) {
 
 
 undersample <- function(train_data, class_val, class_data, eval_fun, seed) {
-    sample_decrement = 0.1
-    increment_minimum = 0.05
+    sample_decrement <- 0.1
+    increment_minimum <- 0.05
     if (seed) {set.seed(2292)}
     other_classes <- train_data[train_data[,ncol(train_data)] != class_val,]
     curr_class <- train_data[train_data[,ncol(train_data)] == class_val,]
@@ -81,7 +87,7 @@ undersample <- function(train_data, class_val, class_data, eval_fun, seed) {
     new_train <- rbind(other_classes, curr_class)
     new_acc <- evaluate_model(eval_fun, new_train)
     for (i in ncol(class_val)) {
-        if (class_data['maj', i] & 
+        if (class_data['orig', i] != -1 & 
             ((class_data['base_val',i] - new_acc[i]) / 
              class_data['base_val',i] > increment_minimum)) {
             return(train_data)
@@ -95,11 +101,38 @@ undersample <- function(train_data, class_val, class_data, eval_fun, seed) {
 
 
 wrap_smote <- function(class_data, train_data, eval_fun) {
-    # returns list
+    # returns new training data set
     # element 1: updated class_data
     # element 2: updated train_data
-    return(list(class_data, train_data))
+
+    # Set StopSampling Flag to False
+    class_data <- rbind(class_data, stop_sampling = !class_data['min',])
+    
+    # main loop
+    while (sum(class_data['stop_sampling'] < ncol(class_data))) {
+        for (i in 1:ncol(class_data)) {
+            if (class_data['stop_sampling',i] == FALSE) {
+                train_len = nrow(train_data)
+                train_data <- smote(train_data, 
+                                          colnames(class_data)[i],
+                                          class_data,
+                                          eval_fun,
+                                          seed)
+                if (nrow(train_data) == train_len) {
+                    class_data['stop_sampling',i] <- TRUE
+                }
+            }
+        }
+    }
+    return(train_data)
 }
+
+
+smote <- function(foo) {
+    sample_increment <- 100
+    look_ahead_value <- 3
+}
+
 
 evaluate_model <- function(eval_fun, data_set) {
     # runs prediction model & returns vector of class specificity values
