f8bdaf1eee216c99a07a2b306c70ec21d0b5d4fc
asterix135 <chgraham@gmail.com>
2015-12-02 20:14:01 -0500

    under/over written - needs to be tested

--- Assignment3/over_under_wrapper.R
+++ Assignment3/over_under_wrapper.R
@@ -79,25 +79,30 @@ wrap_undersample <- function(class_data, train_data, eval_fun, seed) {
 undersample <- function(train_data, class_val, class_data, eval_fun, seed) {
     sample_decrement <- 0.1
     increment_minimum <- 0.05
-    if (seed) {set.seed(2292)}
+    # split out class we are lookig at
     other_classes <- train_data[train_data[,ncol(train_data)] != class_val,]
     curr_class <- train_data[train_data[,ncol(train_data)] == class_val,]
+    if (seed) {set.seed(2292)}
+    # undersample by decrement level
     curr_class <- curr_class[sample(nrow(curr_class), 
                                     floor(nrow(curr_class) * 
                                               (1-sample_derement))),]
     new_train <- rbind(other_classes, curr_class)
-    new_acc <- evaluate_model(eval_fun, new_train)
-    for (i in ncol(class_val)) {
+    # evaluate f-values for new data set
+    # return new data unless decrease minority values or decrease majority 
+    #   values by more than increment_minimum
+    new_f <- evaluate_model(eval_fun, new_train)
+    for (i in 1:ncol(class_val)) {
         if (class_data['orig', i] != -1 & 
-            ((class_data['best_f',i] - new_acc[i]) / 
+            ((class_data['best_f',i] - new_f[i]) / 
              class_data['best_f',i] > increment_minimum)) {
-            return(train_data)
+            return(train_data, class_data)
         } else if (class_data['min', i] & 
-                   (class_data['best_f', i] > new_acc[i])) {
-            return(train_data)
+                   (class_data['best_f', i] > new_f[i])) {
+            return(train_data, class_data)
         } else {
             class_data['best_f',] <- pmax(as.numeric(class_data['best_f',]), 
-                                          new_acc) 
+                                          new_f) 
         }
     }
     return(list(new_train, class_data))
@@ -117,11 +122,12 @@ wrap_smote <- function(class_data, train_data, eval_fun, seed) {
             if (class_data['stop_sampling',i] == FALSE) {
                 train_len = nrow(train_data)
                 updated <- smote_sample(train_data, 
-                                       colnames(class_data)[i],
-                                       class_data,
-                                       eval_fun,
-                                       seed)
-                train_data <- updated[[1]]
+                                        synth_data,
+                                        colnames(class_data)[i],
+                                        class_data,
+                                        eval_fun,
+                                        seed)
+                train_data <- rbind(train_data, updated[[1]])
                 class_data <- updated[[2]]
                 if (nrow(train_data) == train_len) {
                     class_data['stop_sampling',i] <- TRUE
@@ -134,27 +140,54 @@ wrap_smote <- function(class_data, train_data, eval_fun, seed) {
 
 
 smote_sample <- function(train_data, class_val, class_data, eval_fun, seed) {
-    sample_increment <- 100
     look_ahead_value <- 3
-}
-
-
-smote <- function(minor_class, train_data) {
-    k <- 6
-    if (seed) {set.seed(2292)}
     other_classes <- train_data[train_data[,ncol(train_data)] != class_val,]
     curr_class <- train_data[train_data[,ncol(train_data)] == class_val,]
-    synth_class <- data.frame(matrix)
-        
+    # NEED TO ADD PARAMETERS!!!!!!!!!!!!!!!!!!!!!
+    synth_class <- smote(curr_class, train_data, seed)
+    # Evaluate if new model improves f-values by at least 5%
+    new_f <- evaluate_model(eval_fun, rbind(train_data, synth_class))
+    for (i in 1: nrow(class_data)) {
+        if (class_data['min', i] & 
+            ((new_f[i] - class_data['best_f',i] / 
+             class_data['best_f',i] < increment_minimum))) {
+            return(train_data, class_list)
+        } else {
+            class_data['best_f',] <- pmax(as.numeric(class_data['best_f',]), 
+                                          new_f) 
+        }
+    }
+    return(list(rbind(new_train, synth_data), class_data))
 }
 
 
-populate <- function(os_rate, class_rep, near_nbrs, k) {
-    # os_rate is an integer representation of % oversample rate (1 = 100% etc)
-    # class_rep is the class member being replicated
+smote <- function(minor_class, train_data, seed) {
+    k <- 6
+    synth_data <- data.frame(matrix(NA, nrow=0, ncol = ncol(minor_class)))
+    col_names(synth_data) <- colnames(minor_class)
+    if (seed) {set.seed(2292)}
+    # get indexes of knn records for each minor class rep
+    nbr_idxs <- get.knnx(train_data[,1:(ncol(train_data)-1)], 
+                         minor_class[,1:(ncol(train_data)-1)], k=k)[[1]]
+    # Loop through each minor class
+    for (i in 1:nrow(minor_class)) {
+        # get train_data 2:k and put into a df (assume best match is itself)
+        near_nbrs <- train_data[nbr_idxs[i,2:6]]
+        # change minor class into a vector
+        class_rep <- as.numeric(minor_class[i,])
+        synth_data <- rbind(synth_data, populate(class_rep, near_nbrs, k))
+    }
+    return(synth_data)    
+}
+
+
+populate <- function(class_rep, near_nbrs, k) {
+    # class_rep is the class member being replicated as a vector
     ## near_nbrs is a data frame of the k nearest neighbours of class_rep
     # This implementation assumes all predictor variables are continuous
-
+    
+    os_rate <- 1  # oversample rate (1 = 100%)
+    
     new_samples <- data.frame(matrix(NA, nrow = 0, ncol = ncol(near_nbrs)))
     colnames(new_samples) <- colnames(near_nbrs)
     while (os_rate > 0) {
